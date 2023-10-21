cba69903497970a69f378c43f096a8b067932fad
docyx <oliverxur@gmail.com>
2023-10-20 20:58:35 -0400

    [css] new font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,ts,json}'],
   theme: {
+    fontFamily: {
+      sans: ['Rubik', ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
